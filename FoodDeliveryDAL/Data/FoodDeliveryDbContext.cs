6f422fff114ae961180991d05c502d9bdcc0f52d
Petr Janik <peta.janik@email.cz>
2022-09-24 09:17:29 +0200

    Setup dev and prod databases

--- FoodDeliveryDAL/Data/FoodDeliveryDbContext.cs
+++ FoodDeliveryDAL/Data/FoodDeliveryDbContext.cs
@@ -1,18 +1,29 @@
-﻿using FoodDeliveryDAL.Models;
+﻿using System.Configuration;
+using FoodDeliveryDAL.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace FoodDeliveryDAL.Data;
 
 public class FoodDeliveryDbContext : DbContext
 {
-    private const string DatabaseName = "PizzaShop";
-    private const string ConnectionString = $"Host=localhost;Database={DatabaseName}";
-
     public DbSet<Order> Orders { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        optionsBuilder.UseNpgsql(ConnectionString);
+        var environment = ConfigurationManager.AppSettings["Environment"];
+        var connectionString = environment switch
+        {
+            null => throw new Exception("Missing environment."),
+            "Development" => ConfigurationManager.AppSettings["ConnectionStringDevelopment"],
+            "Production" => ConfigurationManager.AppSettings["ConnectionStringProduction"],
+            _ => throw new Exception($"Invalid environment {environment}")
+        };
+        if (connectionString is null)
+        {
+            throw new Exception("Missing connection string.");
+        }
+
+        optionsBuilder.UseNpgsql(connectionString);
     }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
