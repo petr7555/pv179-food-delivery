0d8c64c57e66274b178273f685099625ca49d5e9
Oliver Svetlík <xsvetli1@fi.muni.cz>
2022-10-02 14:54:00 +0200

    Update Order Model

--- FoodDeliveryDAL/Models/Order.cs
+++ FoodDeliveryDAL/Models/Order.cs
@@ -1,6 +1,17 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace FoodDeliveryDAL.Models;
 
 public class Order: BaseEntity
 {
-    public string Note { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public int RatingId { get; set; }
+    [ForeignKey(nameof(RatingId))]
+    public virtual Rating Rating { get; set; }
+    public int CustomerId { get; set; }
+    [ForeignKey(nameof(CustomerId))]
+    public virtual CustomerDetails Customer { get; set; }
+    public int PaymentMethodId { get; set; }
+    [ForeignKey(nameof(PaymentMethodId))]
+    public virtual PaymentMethod PaymentMethod { get; set; }
 }
