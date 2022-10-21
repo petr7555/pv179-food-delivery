1ec69ca96f0246128e65dd5ce70eb5a15414755c
Petr Janik <peta.janik@email.cz>
2022-10-21 12:46:48 +0200

    Explicitly define result of GetByIdAsync as nullable

--- FoodDelivery.Infrastructure/Repository/IRepository.cs
+++ FoodDelivery.Infrastructure/Repository/IRepository.cs
@@ -2,7 +2,7 @@ namespace FoodDelivery.Infrastructure.Repository;
 
 public interface IRepository<TEntity, in TKey> where TEntity : class where TKey : struct
 {
-    public Task<TEntity> GetByIdAsync(TKey id);
+    public Task<TEntity?> GetByIdAsync(TKey id);
     public Task<IEnumerable<TEntity>> GetAllAsync();
     public void Create(TEntity entity);
     public void Update(TEntity entity);
