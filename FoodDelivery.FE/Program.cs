dcbf0e6f273638b8c029808653be46ab3bcb36d3
Petr Janik <peta.janik@email.cz>
2022-11-07 17:08:57 +0100

    Remove debug things

--- FoodDelivery.FE/Program.cs
+++ FoodDelivery.FE/Program.cs
@@ -1,13 +1,9 @@
 using FoodDelivery.DAL.EntityFramework.Data;
-using FoodDelivery.DAL.EntityFramework.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
-
-var keyVaultConnectionString = builder.Configuration.GetConnectionString("KeyVaultConnectionString");
-
 var app = builder.Build();
 
 // TODO later add development switch
@@ -18,16 +14,6 @@ using (var db = new FoodDeliveryDbContext())
     Console.WriteLine("Resetting database...");
     db.Database.EnsureDeleted();
     db.Database.EnsureCreated();
-    db.Restaurants.Add(new Restaurant
-    {
-        Name = "Pizza" + keyVaultConnectionString,
-        DeliveryPrice = new Price
-        {
-            Amount = 1.2f,
-            Currency = new Currency { Name = "aaa" },
-        }
-    });
-    db.SaveChanges();
 }
 // }
 
