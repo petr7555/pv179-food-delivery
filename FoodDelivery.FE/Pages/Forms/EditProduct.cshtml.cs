5b39d747cd118c6699b5bda1e100e92f0fbffa64
Petr Janik <peta.janik@email.cz>
2023-01-14 10:47:53 +0100

    Fix incorrect category being shown

--- FoodDelivery.FE/Pages/Forms/EditProduct.cshtml.cs
+++ FoodDelivery.FE/Pages/Forms/EditProduct.cshtml.cs
@@ -10,88 +10,85 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
 
-namespace FoodDelivery.FE.Pages.Forms
+namespace FoodDelivery.FE.Pages.Forms;
+
+public class EditProduct : PageModel
 {
-    public class EditProduct : PageModel
+    [BindProperty]
+    public ProductUpdateDto Product { get; set; }
+
+    [BindProperty]
+    public List<PriceUpdateDto> Prices { get; set; }
+
+    public IEnumerable<CategoryGetDto> Categories { get; set; }
+
+    [BindProperty]
+    public string SelectedCategoryId { get; set; }
+
+    public SelectList TagOptionsCategories { get; set; }
+
+    private readonly IProductFacade _productFacade;
+    private readonly IRestaurantFacade _restaurantFacade;
+
+    private readonly ICategoryService _categoryService;
+    private readonly IPriceService _priceService;
+
+    public EditProduct(IProductFacade productFacade, IRestaurantFacade restaurantFacade,
+        ICategoryService categoryService, IPriceService priceService)
     {
-        [BindProperty]
-        public ProductUpdateDto Product { get; set; }
-        [BindProperty]
-        public List<PriceUpdateDto> Prices { get; set; }
-        public IEnumerable<CategoryGetDto> Categories { get; set; }        
+        _productFacade = productFacade;
+        _restaurantFacade = restaurantFacade;
+        _categoryService = categoryService;
+        _priceService = priceService;
+    }
 
-        [BindProperty]
-        public string SelectedTagCategory { get; set; }
-
-        public SelectList TagOptionsCategories { get; set; }
-
-        private readonly IProductFacade _productFacade;
-        private readonly IRestaurantFacade _restaurantFacade;
-
-        private readonly ICategoryService _categoryService;
-        private readonly IPriceService _priceService;
-
-        public EditProduct(IProductFacade productFacade, IRestaurantFacade restaurantFacade, ICategoryService categoryService, IPriceService priceService)
+    public async Task OnGet(Guid productId, Guid restaurantId)
+    {
+        if (productId != Guid.Empty)
         {
-            _productFacade = productFacade;
-            _restaurantFacade = restaurantFacade;
-            _categoryService = categoryService;
-            _priceService = priceService;
+            Product = await _productFacade.GetByIdAsyncAsUpdateDto(productId);
+            Prices = Product.Prices;
+            SelectedCategoryId = Product.CategoryId.ToString();
         }
-        public async Task OnGet(Guid productId, Guid restaurantId)
+        else
         {
-            if (productId != Guid.Empty)
+            Product = new ProductUpdateDto();
+            Product.RestaurantId = restaurantId;
+            Prices = new List<PriceUpdateDto>();
+            var currencies = await _priceService.GetAllCurrencies();
+            foreach (var currency in currencies)
             {
-                Product = await _productFacade.GetByIdAsyncAsUpdateDto(productId);
-                Prices = Product.Prices;
+                var price = new PriceUpdateDto();
+                price.CurrencyId = currency.Id;
+                price.Currency = currency;
+                Prices.Add(price);
             }
-            else
-            {
-                Product = new ProductUpdateDto();
-                Product.RestaurantId = restaurantId;
-                Prices = new List<PriceUpdateDto>();
-                var currencies = await _priceService.GetAllCurrencies();
-                foreach (var currency in currencies)
-                {
-                    var price = new PriceUpdateDto();
-                    price.CurrencyId = currency.Id;
-                    price.Currency = currency;
-                    Prices.Add(price);
-                }                
-            }
-
-            Categories = await _categoryService.GetAllAsync();
-            TagOptionsCategories = new SelectList(Categories.ToList(), nameof(CategoryGetDto.Id), nameof(CategoryGetDto.Name));
         }
 
-        public async Task<IActionResult> OnPost()
+        Categories = await _categoryService.GetAllAsync();
+        TagOptionsCategories =
+            new SelectList(Categories.ToList(), nameof(CategoryGetDto.Id), nameof(CategoryGetDto.Name));
+    }
+
+    public async Task<IActionResult> OnPost()
+    {
+        Product.CategoryId = new Guid(SelectedCategoryId);
+        if (Product.Id == Guid.Empty)
         {
-            Product.CategoryId = new Guid(SelectedTagCategory);
-            if (Product.Id == Guid.Empty)
+            Product.Id = Guid.NewGuid();
+            foreach (var price in Prices)
             {
-                Product.Id = Guid.NewGuid();
-                foreach (var price in Prices)
-                {
-                    price.Id = Guid.NewGuid();
-                    price.ProductId = Product.Id;
-                }
-                await _productFacade.CreateWithNewPrices(Product, Prices);
+                price.Id = Guid.NewGuid();
+                price.ProductId = Product.Id;
             }
-            else
-            {
-                await _productFacade.UpdateAsync(Product, Prices);
-            }
-            return RedirectToPage("/Lists/ProductList", new { restaurantId = Product.RestaurantId });
+
+            await _productFacade.CreateWithNewPrices(Product, Prices);
+        }
+        else
+        {
+            await _productFacade.UpdateAsync(Product, Prices);
         }
 
-        public async Task<string> GetCategoryName()
-        {
-            if (Product.CategoryId == Guid.Empty)
-            {
-                return "";
-            }
-            var category = await _categoryService.GetByIdAsync(Product.CategoryId);
-            return category.Name;
-        }
+        return RedirectToPage("/Lists/ProductList", new { restaurantId = Product.RestaurantId });
     }
 }
